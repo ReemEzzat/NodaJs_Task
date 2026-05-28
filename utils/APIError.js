d997bdd7eb4b6dae92878b8029c4e28dd4be1a7d
ReemEzzat <remaezzat.exo@gmail.com>
2026-05-29 00:40:14 +0300

    add error file

---
+++ utils/APIError.js
@@ -0,0 +1,9 @@
+class APIError extends Error {
+    constructor(message, status) {
+        super(message);
+        this.name = "APIError";
+        this.status = status;
+    }
+}
+
+module.exports = APIError;
